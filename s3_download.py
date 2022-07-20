05ce9b8ac35dbe0c4fc8b3fc06edf059bde42cc9
syKim12 <sooyeon@lgcns.com>
2022-07-20 11:11:00 +0900

    upload s3 download

---
+++ s3_download.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+
+with DAG(
+    dag_id='s3_download',
+    schedule_interval='@daily',
+    start_date=datetime(2022, 3, 1),
+    catchup=False
+) as dag:
+    pass
+
+def download_from_s3(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+    
+    
+with DAG(...) as dag:
+    # Download a file
+    task_download_from_s3 = PythonOperator(
+        task_id='download_from_s3',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'posts.json',
+            'bucket_name': 'bds-airflow-bucket',
+            'local_path': '/Users/dradecic/airflow/data/'
+        }
+    )
+
+def rename_file(ti, new_name: str) -> None:
+    downloaded_file_name = ti.xcom_pull(task_ids=['download_from_s3'])
+    downloaded_file_path = '/'.join(downloaded_file_name[0].split('/')[:-1])
+    os.rename(src=downloaded_file_name[0], dst=f"{downloaded_file_path}/{new_name}")
+    
+    
+with DAG(...) as dag:
+    # Download a file
+    task_download_from_s3 = PythonOperator(...)
+    
+    # Rename the file
+    task_rename_file = PythonOperator(
+        task_id='rename_file',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': 's3_downloaded_posts.json'
+        }
+    )
+
+    task_download_from_s3 >> task_rename_file
